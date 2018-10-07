9a2b97d4a53c1f16245efda10a465802a0f55af3
Alex Lo <hongtzang@gmail.com>
2018-10-07 17:37:57 -0400

    fixy fix

--- src/main/java/ca/mcgill/ecse321/rideshare/passengerController/PassengerService.java
+++ src/main/java/ca/mcgill/ecse321/rideshare/passengerController/PassengerService.java
@@ -7,18 +7,18 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import ca.mcgill.ecse321.rideshare.model.Car;
-import ca.mcgill.ecse321.rideshare.model.Driver;
 import ca.mcgill.ecse321.rideshare.model.Passenger;
+import ca.mcgill.ecse321.rideshare.model.User;
 
 @Service
 public class PassengerService {
 	Car car1 = new Car();
 	private List<Passenger> passengers = new ArrayList<>(Arrays.asList(
-			new Passenger("Driver1", "ddsdsds", 2, "donat@test.com", "01233445", "Alien",
+			new Passenger("Passenger1", "ddsdsds", 2, "donat@test.com", "01233445", "Alien",
 					"Mars", "myUsername", true, "55555555555"),
-			new Passenger("Driver2", "Lassdfadtname2", 55, "test@test.com", "555555", "Earthling",
+			new Passenger("Passenger2", "Lassdfadtname2", 55, "test@test.com", "555555", "Earthling",
 					"Earth", "another username", true, "7777777777777"),
-			new Passenger("Driver3", "ssssssss", 901, "earth@test.com", "01233445", "Earthling",
+			new Passenger("Passenger3", "ssssssss", 901, "earth@test.com", "01233445", "Earthling",
 					"Pacific Ocean", "sdfd", true, "8888888888888")
 			));
 	
