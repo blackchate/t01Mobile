df5fbe19a4c10eaff375f45576581d1164f333d8
donatKapesa <donatkap14@gmail.com>
2018-10-28 18:40:24 -0400

    design for browse and history tab

--- Rideshareapplication/app/src/main/java/ca/mcgill/cs/ecse321/rideshareapplication/MainActivity.java
+++ Rideshareapplication/app/src/main/java/ca/mcgill/cs/ecse321/rideshareapplication/MainActivity.java
@@ -1,13 +1,14 @@
 package ca.mcgill.cs.ecse321.rideshareapplication;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // setting up bottom nav on start up
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new BrowseTripsFragment()).commit(); // opens browse_trips fragment first
         bottomNav.getMenu().getItem(1).setChecked(true); // on startup, Browse Trips is selected
+
+////        // setting up ListView for browse trips
+//        ListView myListView;
+//
+////        // these will all come from trip objects from the server
+//        String[] sources;
+//        String[] destinations;
+//        String[] prices;
+//
+//        Resources res = getResources();
+//
+//        myListView = (ListView)findViewById(R.id.myListView);
+//        sources = res.getStringArray(R.array.sources);
+//        destinations = res.getStringArray(R.array.destinations);
+//        prices = res.getStringArray(R.array.prices);
+//
+//        myListView.setAdapter(new ArrayAdapter<String>(this, R.layout.browse_trip_textview, sources));
+//
+//        /*
+//        ItemAdapter itemAdapter = new ItemAdapter(this, sources, destinations, prices);
+//        myListView.setAdapter(itemAdapter);
+//        */
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
