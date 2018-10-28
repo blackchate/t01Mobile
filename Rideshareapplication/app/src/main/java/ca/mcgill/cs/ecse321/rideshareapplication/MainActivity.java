cbac602112d7d180ba59ebbb2c0ad5b41c5c27af
donatKapesa <donatkap14@gmail.com>
2018-10-27 20:20:29 -0400

    major redesign

--- Rideshareapplication/app/src/main/java/ca/mcgill/cs/ecse321/rideshareapplication/MainActivity.java
+++ Rideshareapplication/app/src/main/java/ca/mcgill/cs/ecse321/rideshareapplication/MainActivity.java
@@ -3,38 +3,46 @@ package ca.mcgill.cs.ecse321.rideshareapplication;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new BrowseTripsFragment()).commit(); // opens browse_trips fragment first
+        bottomNav.getMenu().getItem(1).setChecked(true); // on startup, Browse Trips is selected
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) { // item that was selected
+                    Fragment selectedFragment = null; // fragment that we want to open
+
+                    switch(menuItem.getItemId()) {
+                        case R.id.nav_browse_trips:
+                            selectedFragment = new BrowseTripsFragment();
+                            break;
+                        case R.id.nav_booked_trip:
+                            selectedFragment = new BookedTripFragment();
+                            break;
+                        case R.id.nav_history:
+                            selectedFragment = new HistoryFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true; // when we return the fragment, nav item showing the fragment is selected as well
+                }
+            };
 }
