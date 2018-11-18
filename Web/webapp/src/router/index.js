2bf9fde22fdd795113349fb6c1a67f8f81ec4ff9
COMP202_Shayan_Sheikh <shayan.sheikh@mail.mcgill.ca>
2018-11-18 05:58:36 -0500

    updated index file: added backend url

--- Web/webapp/src/router/index.js
+++ Web/webapp/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
+import FleetStatus from '@/components/FleetStatus'
+import Rankings from '@/components/Rankings'
 
 Vue.use(Router)
 
@@ -13,3 +15,23 @@ Vue.use(Router)
 //     }
 //   ]
 // })
+
+export default new Router({
+    routes: [
+      {
+        path: '/',
+        name: 'FleetStatus',
+        component: FleetStatus
+      },
+      {
+        path: '/User',
+        name: 'User',
+        component: User
+      },
+      {
+        path: '/Rankings',
+        name: 'Rankings',
+        component: Rankings
+      }
+    ]
+  })
